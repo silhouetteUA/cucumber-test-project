07dec5d772e296ab1245f32c384363050372a716
Evgeniy Dmitriev <dmitriev.evgeniy@gmail.com>
2022-12-06 20:44:44 -0500

    four

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -1,6 +1,30 @@
 Feature: Application Login
-  Scenario: Home page default login
+  Scenario: Home page default login 1
     Given User is on banking landing page
-    When User login to application with username and password
+    When User login to application with username "Max" and password "Max123"
     Then Homepage is populated
-    And users cards are displayed
+    And cards are displayed "yes"
+
+  Scenario: Home page default login 2
+    Given User is on banking landing page
+    When User login to application with username "Jay" and password "Jay123"
+    Then Homepage is populated
+    And cards are displayed "no"
+
+#  Scenario: Home page default login 3
+#    Given User is on banking landing page
+#    When User login to application with the details
+#      | username_jenny | password_123 | email@gmail.com | tel_380632103372 | address_Ukraine |
+#    Then Homepage is populated
+#    And cards are displayed "no"
+
+  Scenario Outline: multiple values
+    Given User is on banking landing page
+    When Account login with <username> and password <password> and email <email>
+    Then Homepage is populated
+    And cards are displayed "no"
+    Examples:
+      | username | password | email |
+      | user1    | pass1    | mail1 |
+      | user2    | pass2    | mail2 |
+      | user3    | pass3    | mail3 |
--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -1,9 +1,13 @@
 package stepDefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import java.util.Collections;
+import java.util.List;
 
 public class stepDefinition {
 
@@ -25,5 +29,35 @@ public class stepDefinition {
     @Then("Homepage is populated")
     public void homepageIsPopulated() {
         System.out.println("Logged in: I see my homepage");
+    }
+
+
+    @When("User login to application with username {string} and password {string}")
+    public void userLoginToApplicationWithUsernameAndPassword(String username, String password) {
+        System.out.println("username = " + username + ";  password = " +password + ";");
+    }
+
+    @And("users cards are not displayed")
+    public void usersCardsAreNotDisplayed() {
+        System.out.println("Nothing to see here ...");
+    }
+
+    @And("cards are displayed {string}")
+    public void cardsAreDisplayed(String attribute) {
+        System.out.println("cards are displayed: " + attribute);
+    }
+
+    @When("User login to application with the details")
+    public void userLoginToApplicationWithTheDetails(DataTable table) {
+        List<List<String>> obj = Collections.singletonList(table.asList());
+        System.out.println("name: "+ obj.get(0).get(0));
+    }
+
+
+    @When("^Account login with (.+) and password (.+) and email (.+)$")
+    public void accountLoginWithUsernameAndPasswordPasswordAndEmailEmail(String login, String pass, String email) {
+        System.out.println("login: " + login);
+        System.out.println("pass: " + pass);
+        System.out.println("email: " + email);
     }
 }
